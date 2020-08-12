3cf336485402af6ed964bcf6893d2cfa7e7622e8
IvanMi773 <imihajlovskij23@gmail.com>
2020-08-12 13:22:29 +0300

    many to many relationship article with category

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Category;
+use App\Models\Article;
+
 class HomeController extends Controller
 {
     /**
@@ -23,6 +26,24 @@ class HomeController extends Controller
      */
     public function index()
     {
+		$categories = ['Office Chairs', 'Modern Chairs', 'Home Chairs'];
+
+		foreach($categories as $category)
+		{
+			Category::create([
+				'name'  =>  $category,
+			]);
+		}
+
+		$article = Article::create([
+			'title'  =>  'Home Brixton Faux Leather Armchair',
+			'slug' =>  'fadjsklfas',
+			'text' =>  'fadjsklfas',
+		]);
+		
+		$categories = Category::find([2,3]); // Modren Chairs, Home Chairs
+		$article->categories()->attach($categories);
+
         return view('home');
     }
 }
--- app/Models/Article.php
+++ app/Models/Article.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title', 'slug', 'text'
+	];
+	
+	public function categories ()
+	{
+		return $this->belongsToMany(Category::class);
+	}
 }
