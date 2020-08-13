f0466d881cdee8f4d19e4e3c39f65d72fef13071
IvanMi773 <imihajlovskij23@gmail.com>
2020-08-13 15:21:48 +0300

    change title font, cateogries output

--- app/Http/Controllers/Blog/ArticleController.php
+++ app/Http/Controllers/Blog/ArticleController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Blog;
 
 use Illuminate\Http\Request;
 use App\Models\Article;
+use App\Models\Category;
 use App\Http\Controllers\Controller;
 
 class ArticleController extends Controller
@@ -12,8 +13,9 @@ class ArticleController extends Controller
 	{
 		// TODO: count of words in text
 		$articles = Article::where('is_dirt', '=', '0')->paginate(20);
+		$categories = Category::all();
 		
-		return view('blog.articles.index', compact('articles'));
+		return view('blog.articles.index', compact('articles', 'categories'));
 	}
 
 	public function show (Article $article) 
--- resources/views/blog/articles/index.blade.php
+++ resources/views/blog/articles/index.blade.php
@@ -2,16 +2,23 @@
 
 @section('content')
 	<div class="centered-block">
+		@foreach($categories as $category)
+			<a href="#" class="category">
+				{{ $category->name }} <span class="dash"> | </span>
+			</a>
+		@endforeach
+
+		<br>
+		<hr>
+
 		@foreach($articles as $article)
-			<a href="{{ route('article.show', $article->id) }}" class="text-left text-black text-break">
-				<h2>
-					{{ $article->title }}
-				</h2>
+			<a href="{{ route('article.show', $article->id) }}" class="text-left title text-break">
+				{{ $article->title }}
 			</a>
 			
-			<p class="text-muted mb-0">{{ $article->created_at }}</p>
+			<p class="text-muted mb-3 mt-3">{{ $article->created_at }}</p>
 
-			<p class="text-break text mt-0">
+			<p class="text-break text mb-5">
 				{{ $article->slug }}
 				
 				<a href="{{ route('article.show', $article->id) }}" class="text-left text-break">Continue...</a>
